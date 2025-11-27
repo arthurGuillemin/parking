654f58bd0bf5f2cb0f56713cce118f1bcdd414c2
Arthur Guillemin <aguillemin291@gmail.com>
2025-11-27 21:40:15 +0100

    add phpUnit dependencie and create tests for database singleton

--- src/Infrastructure/Database/database.php
+++ src/Infrastructure/Database/database.php
@@ -23,11 +23,23 @@ class Database
     {
         if (self::$instance === null) {
 
-            $host = $_ENV['DB_HOST'];
-            $db   = $_ENV['DB_NAME'];
-            $user = $_ENV['DB_USER'];
-            $pass = $_ENV['DB_PASSWORD'];
-            $port = $_ENV['DB_PORT'];
+            // opur les tests sqlite 
+            if (!empty($_ENV['APP_ENV']) && $_ENV['APP_ENV'] === 'test') {
+                self::$instance = new PDO('sqlite::memory:');
+                self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                return self::$instance;
+            }
+
+            // en prod supabase
+            $host = $_ENV['DB_HOST'] ?? null;
+            $db   = $_ENV['DB_NAME'] ?? null;
+            $user = $_ENV['DB_USER'] ?? null;
+            $pass = $_ENV['DB_PASSWORD'] ?? null;
+            $port = $_ENV['DB_PORT'] ?? 5432;
+
+            if (!$host || !$db || !$user) {
+                throw new \RuntimeException("Missing required database environment variables");
+            }
 
             $dsn = "pgsql:host=$host;port=$port;dbname=$db";
 
