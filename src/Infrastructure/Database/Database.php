334d857b1d8f0aeec573d5832655d1b1a60e101f
Arthur Guillemin <aguillemin291@gmail.com>
2025-12-22 19:31:15 +0100

    fix(env): prevent dotenv crash when .env is missing

--- src/Infrastructure/Database/Database.php
+++ src/Infrastructure/Database/Database.php
@@ -10,7 +10,7 @@ require __DIR__ . '/../../../vendor/autoload.php';
 use Dotenv\Dotenv;
 
 $dotenv = Dotenv::createImmutable(dirname(__DIR__, 3));
-$dotenv->load();
+$dotenv->safeLoad();
 
 class Database
 {
